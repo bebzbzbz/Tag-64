b81f3d93b211e3dc26d186cfb67b9cfee394d277
bebzbzbz <balzerbeatrice@gmail.com>
2025-02-25 15:10:04 +0100

    correction

--- src/components/DayNight.tsx
+++ src/components/DayNight.tsx
@@ -4,7 +4,7 @@ function DayNight() {
     const [isDay, setIsDay] = useState(true)
 
     return(
-        <div className={`${!isDay ? "night" : ""} daynight`}>
+        <div className={`${!isDay && "night"} daynight`}>
             <h1>{isDay ? "Day" : "Night"}</h1>
             <button onClick={() => setIsDay(!isDay)}>Change to {isDay ? "Night" : "Day"}</button>
         </div>
